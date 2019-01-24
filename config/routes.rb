556dde975d45ab6792935be019ad66e72e3b56df
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2019-01-24 12:09:54 -0500

    all base controllers made

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/new'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
