c99b53e023ef86b94f8d8a72a3a521b4bc28f1a7
ctobiasz <tobiasz.christopher@gmail.com>
2019-04-28 02:13:09 -0400

    starting fresh

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,7 +9,7 @@ module Puppymeetup
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.secret_key_base = "a99e4ede177593d3c5666cfdf4eddb4eb6aabac59fa66362222ce17dec997e42599b6f340321c5c63810a3ffd5c51968cd570054136391b184c32151b7c21b39"
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
